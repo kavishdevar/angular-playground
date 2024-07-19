740f387802103bf7d29f3c3b532334fd9b143220
Kavish Devar <mail@kavishdevar.me>
2024-07-19 18:07:49 +0530

    finish up with the UI

--- hammingcode/src/app/app.component.ts
+++ hammingcode/src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, MatInputModule, MatFormFieldModule, MatButtonModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor() {
+    document.addEventListener('DOMContentLoaded', () => {
+      document.querySelectorAll('input').forEach((input) => {
+        let prevVal = "";
+        input.addEventListener('input', function (e) {
+          if (this.checkValidity()) {
+            prevVal = this.value;
+          } else {
+            this.value = prevVal;
+          }
+        })
+      });
+    }
+    );
+  }
 }
